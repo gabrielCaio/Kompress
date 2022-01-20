ab8c8d9c9f59b6285745f8dcc8db84d67c606832
tauanrafael <rafael.tauan@hotmail.com>
2022-01-20 19:50:50 -0300

    feat: login screen finished

--- mobile/lib/app/pages/login.dart
+++ mobile/lib/app/pages/login.dart
@@ -1,14 +1,150 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: deprecated_member_use
 
-// Programar pagina de login aqui
+import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<Login> {
+  String email = '';
+  String senha = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Login"),
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xff5B04D4),
+                    Color(0xff1B0140),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox.expand(
+              child: Image.asset(
+                'assets/Background.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(35),
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 170,
+                        child: Image.asset('assets/logo_solid_black.png'),
+                      ),
+                      const Text(
+                        'Kompress',
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      ),
+                      const SizedBox(height: 100),
+                      const TextField(
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          labelText: 'E-mail',
+                          labelStyle: TextStyle(color: Colors.white),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      const TextField(
+                        obscureText: true,
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                            labelText: 'Senha',
+                            labelStyle: TextStyle(color: Colors.white),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.blue,
+                            ))),
+                      ),
+                      const SizedBox(height: 120),
+                      SizedBox(
+                        child: RaisedButton(
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          padding: const EdgeInsets.all(0.0),
+                          child: Ink(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                                Color(0xff830FD1),
+                                Color(0xff17BA96)
+                              ]),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30)),
+                            ),
+                            child: Container(
+                              constraints: const BoxConstraints(
+                                maxWidth: 320.0,
+                                minHeight: 50.0,
+                              ), // min sizes for Material buttons
+                              alignment: Alignment.center,
+                              child: const Text(
+                                'Login',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 19,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Ainda não possui uma conta? ',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/register');
+                            },
+                            child: const Text(
+                              'Cadastre-se',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
