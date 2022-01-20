d57e0eebc2dbaca2298e78fb74a54aa313c2441c
gabrielCaio <email.caiogabriel@gmail.com>
2022-01-20 02:11:30 -0300

    feat: fixing tagCard and some minor adjustments

--- mobile/lib/app/components/tag_card.dart
+++ mobile/lib/app/components/tag_card.dart
@@ -16,40 +16,40 @@ class TagCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: CustomTheme.white,
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromRGBO(255, 255, 255, 0.5),
-            offset: Offset(4, 6),
-            blurRadius: 4,
-          ),
-        ],
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Expanded(
-        child: Center(
-          child: Column(
-            children: [
-              Icon(
-                TagIcons.addCircle,
-                size: iconSize,
-                color: CustomTheme.lightGrey,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: CustomTheme.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromRGBO(255, 255, 255, 0.5),
+              offset: Offset(4, 6),
+              blurRadius: 4,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              TagIcons.addCircle,
+              size: iconSize,
+              color: CustomTheme.lightGrey,
+            ),
+            // const SizedBox(height: 20),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: CustomTheme.black,
+                fontSize: 18,
               ),
-              const SizedBox(height: 20),
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: CustomTheme.black,
-                  fontSize: 18,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
