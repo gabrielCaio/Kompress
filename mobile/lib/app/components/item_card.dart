1ea03ea672ed0c4be2cc4b3b79e3ab48c53c4092
Welton <welthanos_dev@outlook.com>
2022-01-21 10:31:27 -0300

    feat: home_page; register; fontSize from ItemCard;

--- mobile/lib/app/components/item_card.dart
+++ mobile/lib/app/components/item_card.dart
@@ -96,6 +96,7 @@ class ItemCard extends StatelessWidget {
             style: const TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
+              fontSize: 18
             ),
           ),
         ],
--- mobile/lib/app/pages/home_page.dart
+++ mobile/lib/app/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kompress/app/components/item_card.dart';
 import 'package:kompress/app/components/tag_card.dart';
+import 'package:kompress/app/utils/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,19 +16,105 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: CustomTheme.gradientEnd,
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              // Text("Home Page"),
-              TagCard(),
-              TagCard(),
-              TagCard(),
-            ],
-          ),
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            const SizedBox(height: 10),
+            const Text(
+              'Olá Caio El Troll,',
+              style: TextStyle(
+                color: CustomTheme.white,
+                fontSize: 17,
+                height: 2,
+              ),
+            ),
+            const Text(
+              'Seja Bem Vindo(a)!',
+              style: TextStyle(
+                color: CustomTheme.white,
+                fontSize: 23,
+              ),
+            ),
+            const SizedBox(height: 40),
+            Row(
+              children: const [
+                Icon(
+                  Icons.restore,
+                  color: CustomTheme.white,
+                ),
+                Text(
+                  ' Produtos vistos recentemente...',
+                  style: TextStyle(
+                    color: CustomTheme.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40),
+            SizedBox(
+              height: 210,
+              child: ListView(
+                clipBehavior: Clip.none,
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  ItemCard(
+                      productName: 'Arroz',
+                      quantity: '10',
+                      expireDate: '22/10/2022'),
+                  ItemCard(
+                      productName: 'Arroz',
+                      quantity: '10',
+                      expireDate: '22/10/2022'),
+                  ItemCard(
+                      productName: 'Arroz',
+                      quantity: '10',
+                      expireDate: '22/10/2022'),
+                  ItemCard(
+                      productName: 'Arroz',
+                      quantity: '10',
+                      expireDate: '22/10/2022'),
+                  ItemCard(
+                      productName: 'Arroz',
+                      quantity: '10',
+                      expireDate: '22/10/2022'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              children: const [
+                Icon(
+                  Icons.label_outlined,
+                  color: CustomTheme.white,
+                ),
+                Text(
+                  ' Categorias',
+                  style: TextStyle(
+                    color: CustomTheme.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                clipBehavior: Clip.none,
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  TagCard(),
+                  TagCard(),
+                  TagCard(),
+                  TagCard(),
+                  TagCard(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
