147f6291e6e5579b96be4429145d7fc8d849eca3
Welton <welthanos_dev@outlook.com>
2022-01-22 04:20:43 -0300

    feat: myPantry page; added padding on ItemListCard

--- mobile/lib/app/components/item_list_card.dart
+++ mobile/lib/app/components/item_list_card.dart
@@ -58,59 +58,62 @@ class ItemListCard extends StatelessWidget {
 
     return InkWell(
       onTap: () => onPress,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: height,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: borderColor),
-        ),
-        child: Row(
-          children: [
-            // Image Thumbnail
-            Container(
-              width: MediaQuery.of(context).size.width * 0.3,
-              height: height,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                ),
-                border: Border.fromBorderSide(BorderSide(color: borderColor)),
-                image: decorationImage,
-              ),
-            ),
-
-            // Content
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        productName,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ),
-                      const SizedBox(height: 15),
-                      Text(
-                        "Validade: $expireDate\nQuantidade: $quantity $measureUnity",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                    ],
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: borderColor),
+          ),
+          child: Row(
+            children: [
+              // Image Thumbnail
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: height,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
                   ),
-                  getIcon(),
-                ],
+                  border: Border.fromBorderSide(BorderSide(color: borderColor)),
+                  image: decorationImage,
+                ),
               ),
-            ),
-          ],
+
+              // Content
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          productName,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          "Validade: $expireDate\nQuantidade: $quantity $measureUnity",
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    getIcon(),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- mobile/lib/app/pages/my_pantry.dart
+++ mobile/lib/app/pages/my_pantry.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:kompress/app/components/item_list_card.dart';
+import 'package:kompress/app/utils/theme.dart';
 
 class MyPantry extends StatefulWidget {
   const MyPantry({Key? key}) : super(key: key);
@@ -12,9 +16,152 @@ class _MyPantryState extends State<MyPantry>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return const Scaffold(
-      body: Center(
-        child: Text("MyPantry"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  CustomTheme.gradientStart,
+                  CustomTheme.gradientEnd,
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    const Text(
+                      'Meus Produtos',
+                      style: TextStyle(
+                        color: CustomTheme.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      style: const TextStyle(color: CustomTheme.black),
+                      cursorColor: CustomTheme.black,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: CustomTheme.white,
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: CustomTheme.lightGrey,
+                        ),
+                        labelText: ' Pesquisar ...',
+                        hintText: 'Exemplo: Arroz',
+                        hintStyle: const TextStyle(color: CustomTheme.lightGrey),
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        labelStyle: const TextStyle(
+                          color: CustomTheme.lightGrey,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: CustomTheme.gradientStart,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: CustomTheme.gradientStart,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.filter_list,
+                          color: CustomTheme.white,
+                        ),
+                        Text(
+                          ' Ordenar por ...',
+                          style: TextStyle(
+                            color: CustomTheme.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 102),
+                        Icon(
+                          Icons.label_outlined,
+                          color: CustomTheme.white,
+                        ),
+                        Text(
+                          ' Categorias ...',
+                          style: TextStyle(
+                            color: CustomTheme.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 430,
+                      child: ListView(
+                        padding: EdgeInsets.zero,
+                        scrollDirection: Axis.vertical,
+                        children: const [
+                          ItemListCard(),
+                          ItemListCard(),
+                          ItemListCard(),
+                          ItemListCard(),
+                          ItemListCard(),
+                          ItemListCard(),
+                          ItemListCard(),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    SizedBox(
+                      child: RaisedButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        padding: const EdgeInsets.all(0.0),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              color: CustomTheme.pastelGreen,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Container(
+                            constraints:
+                               const BoxConstraints(maxWidth: 290.0, minHeight: 60.0,),
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: Row(
+                                children: const [
+                                  Icon(Icons.add_circle_outline, color: CustomTheme.white,size: 35,),
+                                  Text(
+                                    ' Adicionar Novo Produto',
+                                    style: TextStyle(
+                                        color: CustomTheme.white, fontSize: 19),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
