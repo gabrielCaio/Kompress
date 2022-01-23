818d97397eb41133369bf3310a73e7233709f672
Welton <welthanos_dev@outlook.com>
2022-01-22 21:03:26 -0300

    style: Added gradient on homePage

--- mobile/lib/app/pages/home_page.dart
+++ mobile/lib/app/pages/home_page.dart
@@ -16,106 +16,122 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      backgroundColor: CustomTheme.gradientEnd,
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: ListView(
-          children: [
-            const SizedBox(height: 10),
-            const Text(
-              'Olá Caio El Troll,',
-              style: TextStyle(
-                color: CustomTheme.white,
-                fontSize: 17,
-                height: 2,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  CustomTheme.gradientStart,
+                  CustomTheme.gradientEnd,
+                ],
               ),
             ),
-            const Text(
-              'Seja Bem Vindo(a)!',
-              style: TextStyle(
-                color: CustomTheme.white,
-                fontSize: 23,
-              ),
-            ),
-            const SizedBox(height: 40),
-            Row(
-              children: const [
-                Icon(
-                  Icons.restore,
-                  color: CustomTheme.white,
-                ),
-                Text(
-                  ' Produtos vistos recentemente...',
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 40),
+                const Text(
+                  'Olá Caio El Troll,',
                   style: TextStyle(
                     color: CustomTheme.white,
-                    fontSize: 16,
+                    fontSize: 17,
+                    height: 2,
+                  ),
+                ),
+                const Text(
+                  'Seja Bem Vindo(a)!',
+                  style: TextStyle(
+                    color: CustomTheme.white,
+                    fontSize: 23,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.restore,
+                      color: CustomTheme.white,
+                    ),
+                    Text(
+                      ' Produtos vistos recentemente...',
+                      style: TextStyle(
+                        color: CustomTheme.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 45),
+                SizedBox(
+                  height: 210,
+                  child: ListView(
+                    clipBehavior: Clip.none,
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      ItemCard(
+                          productName: 'Arroz',
+                          quantity: '10',
+                          expireDate: '22/10/2022'),
+                      ItemCard(
+                          productName: 'Arroz',
+                          quantity: '10',
+                          expireDate: '22/10/2022'),
+                      ItemCard(
+                          productName: 'Arroz',
+                          quantity: '10',
+                          expireDate: '22/10/2022'),
+                      ItemCard(
+                          productName: 'Arroz',
+                          quantity: '10',
+                          expireDate: '22/10/2022'),
+                      ItemCard(
+                          productName: 'Arroz',
+                          quantity: '10',
+                          expireDate: '22/10/2022'),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.label_outlined,
+                      color: CustomTheme.white,
+                    ),
+                    Text(
+                      ' Categorias',
+                      style: TextStyle(
+                        color: CustomTheme.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+                SizedBox(
+                  height: 150,
+                  child: ListView(
+                    clipBehavior: Clip.none,
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      TagCard(),
+                      TagCard(),
+                      TagCard(),
+                      TagCard(),
+                      TagCard(),
+                    ],
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 40),
-            SizedBox(
-              height: 210,
-              child: ListView(
-                clipBehavior: Clip.none,
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  ItemCard(
-                      productName: 'Arroz',
-                      quantity: '10',
-                      expireDate: '22/10/2022'),
-                  ItemCard(
-                      productName: 'Arroz',
-                      quantity: '10',
-                      expireDate: '22/10/2022'),
-                  ItemCard(
-                      productName: 'Arroz',
-                      quantity: '10',
-                      expireDate: '22/10/2022'),
-                  ItemCard(
-                      productName: 'Arroz',
-                      quantity: '10',
-                      expireDate: '22/10/2022'),
-                  ItemCard(
-                      productName: 'Arroz',
-                      quantity: '10',
-                      expireDate: '22/10/2022'),
-                ],
-              ),
-            ),
-            const SizedBox(height: 50),
-            Row(
-              children: const [
-                Icon(
-                  Icons.label_outlined,
-                  color: CustomTheme.white,
-                ),
-                Text(
-                  ' Categorias',
-                  style: TextStyle(
-                    color: CustomTheme.white,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 150,
-              child: ListView(
-                clipBehavior: Clip.none,
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  TagCard(),
-                  TagCard(),
-                  TagCard(),
-                  TagCard(),
-                  TagCard(),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
