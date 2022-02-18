974792ddb22e1beceb9d9efc5150335a1f8220e2
gabrielCaio <email.caiogabriel@gmail.com>
2022-02-17 22:56:13 -0300

    refactor: deleting user model

--- mobile/lib/app/models/user.dart
+++
@@ -1,9 +0,0 @@
-import 'package:kompress/app/models/product_list.dart';
-
-class User {
-  final String name;
-  final String image;
-  final ProductList list;
-
-  User(this.name, this.image, this.list);
-}
